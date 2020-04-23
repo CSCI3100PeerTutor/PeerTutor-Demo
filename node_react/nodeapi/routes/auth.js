2e167cc707c4c014f0ef0f57910465d3f765a8ac
Simontie <simontie@hotmail.com>
2020-04-23 12:16:58 +1200

    Rest password backend implemented
    Backend for reset password implemented, email is sent via simontienz@gmail.com to user email.

--- node_react/nodeapi/routes/auth.js
+++ node_react/nodeapi/routes/auth.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const { signup, signin, signout } = require("../controllers/auth"); // deconstructed so that only need function name
+const { signup, signin, signout, forgotPassword, resetPassword } = require("../controllers/auth"); // deconstructed so that only need function name
 const { userById } = require("../controllers/user");
-const { userSignupValidator } = require("../validators/index");
+const { userSignupValidator, passwordResetValidator} = require("../validators/index");
 const router = express.Router();
+
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
+
+// password forgot and reset routes
+router.put("/forgot-password", forgotPassword);
+router.put("/reset-password", passwordResetValidator, resetPassword);
 
 /** 
  * looking for the parameter userId in the incomin URL
